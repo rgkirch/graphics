76513b5fed99699c0681f76f4146228f89c33a9b
Richie Kirchofer <rgkirch@gmail.com>
2015-08-12 19:56:47 -0400

    following tut

--- first.cpp
+++ first.cpp
@@ -1,3 +1,4 @@
+#define GLEW_STATIC
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <stdio.h>
@@ -17,11 +18,23 @@ int main(void)
 {
     GLFWwindow* window;
 
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "Failed to initialize GLEW" << std::endl;
+		return -1;
+	}
+
 	glfwSetErrorCallback(error_callback);
 
     /* Initialize the library */
     if ( !glfwInit() )
         return -1;
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);

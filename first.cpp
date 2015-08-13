32daabd8bc51582298621366aed0c9c291152e03
Richie Kirchofer <rgkirch@gmail.com>
2015-08-12 22:35:06 -0400

    triangle

--- first.cpp
+++ first.cpp
@@ -3,67 +3,152 @@
 #include <GLFW/glfw3.h>
 #include <stdio.h>
 
+const GLuint WIDTH = 800, HEIGHT = 600;
+
 void error_callback( int error, const char* description )
 {
-	fputs(description, stderr);
+	fprintf( stderr, "%s", description );
+	//fputs( description, stderr );
 }
 
 static void key_callback( GLFWwindow* window, int key, int scancode, int action, int mode )
 {
-	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, GL_TRUE);
+	if ( key == GLFW_KEY_ESCAPE && action == GLFW_PRESS ) {
+		glfwSetWindowShouldClose( window, GL_TRUE );
+	}
 }
 
-int main(void)
+const GLchar* vertexShaderSource = "#version 330 core\n"
+	// declare inputs with the 'in' keyword
+	// set the location to be used later
+	"layout (location = 0) in vec3 position;\n"
+	"void main()\n"
+	"{\n"
+	"gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+	"}\0";
+const GLchar* fragmentShaderSource = "#version 330 core\n"
+	"out vec4 color;\n"
+	"void main()\n"
+	"{\n"
+	"color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"}\n\0";
+
+int main()
 {
-    GLFWwindow* window;
+	GLFWwindow* window;
 
-	glfwSetErrorCallback(error_callback);
+	glfwSetErrorCallback( error_callback );
 
-    /* Initialize the library */
-    if ( !glfwInit() )
-        return -1;
-
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(800, 600, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
-
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
-
-	glewExperimental = GL_TRUE;
-	if( glewInit() != GLEW_OK )
-	{
-		//Problem: glewInit failed, something is seriously wrong.
-		printf( "glewInit() failed, aborting.\n" );
+	/* Initialize the library */
+	if ( !glfwInit() ) {
+		return -1;
 	}
 
-	glViewport(0, 0, 800, 600);
+	// must use exactly version 3
+	glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 3 );
+	glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 3 );
+	glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
+	glfwWindowHint( GLFW_RESIZABLE, GL_FALSE );
 
-	glfwSetKeyCallback(window, key_callback);
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow( WIDTH, HEIGHT, "Hello World", NULL, NULL );
+	if ( !window ) {
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent( window );
 
-	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+	glfwSetKeyCallback( window, key_callback );
 
-    while (!glfwWindowShouldClose(window))
-    {
-		glClear(GL_COLOR_BUFFER_BIT);
-        glfwPollEvents();
+	glewExperimental = GL_TRUE;
+	if( glewInit() != GLEW_OK ) {
+		fprintf( stderr, "glewInit() failed\n" );
+	}
 
-        /* Render here */
+	glViewport( 0, 0, WIDTH, HEIGHT );
 
-        glfwSwapBuffers(window);
-    }
+	glClearColor( 0.2f, 0.3f, 0.3f, 1.0f );
+
+	// Vertex shader
+	GLuint vertexShader = glCreateShader( GL_VERTEX_SHADER );
+	glShaderSource( vertexShader, 1, &vertexShaderSource, NULL );
+	glCompileShader( vertexShader );
+	// Check for compile time errors
+	GLint success;
+	GLchar infoLog[512];
+	glGetShaderiv( vertexShader, GL_COMPILE_STATUS, &success );
+	if ( !success ) {
+		glGetShaderInfoLog( vertexShader, 512, NULL, infoLog );
+		printf( "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n%s\n", infoLog );
+	}
+	// fragment shader
+	GLuint fragmentShader = glCreateShader( GL_FRAGMENT_SHADER );
+	glShaderSource( fragmentShader, 1, &fragmentShaderSource, NULL );
+	glCompileShader( fragmentShader );
+	// Check for compile time errors
+	glGetShaderiv( fragmentShader, GL_COMPILE_STATUS, &success );
+	if ( !success ) {
+		glGetShaderInfoLog( fragmentShader, 512, NULL, infoLog );
+		printf( "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n%s\n", infoLog );
+	}
+	// Link shaders
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader( shaderProgram, vertexShader );
+	glAttachShader( shaderProgram, fragmentShader );
+	glLinkProgram( shaderProgram );
+	// Check for linking errors
+	glGetProgramiv( shaderProgram, GL_LINK_STATUS, &success );
+	if ( !success ) {
+		glGetProgramInfoLog( shaderProgram, 512, NULL, infoLog );
+		printf( "ERROR::SHADER::PROGRAM::LINKING_FAILED\n%s\n", infoLog );
+	}
+	glDeleteShader( vertexShader );
+	glDeleteShader( fragmentShader );
+
+
+	// Set up vertex data (and buffer(s)) and attribute pointers
+	GLfloat vertices[] = {
+		-0.5f, -0.5f, 0.0f, // Left  
+		 0.5f, -0.5f, 0.0f, // Right 
+		 0.0f,	0.5f, 0.0f	// Top	 
+	};
+	// vertex buffer object (VBO)
+	// vertex array object (VAO)
+	GLuint VBO, VAO;
+	glGenVertexArrays( 1, &VAO );
+	glGenBuffers( 1, &VBO );
+	// Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
+	glBindVertexArray( VAO );
+
+	glBindBuffer( GL_ARRAY_BUFFER, VBO );
+	glBufferData( GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW );
+
+	glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0 );
+	glEnableVertexAttribArray( 0 );
+
+	glBindBuffer( GL_ARRAY_BUFFER, 0 ); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
+
+	glBindVertexArray( 0 ); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs)
+
+	// Game loop
+
+	while( !glfwWindowShouldClose( window ) ) {
+		glfwPollEvents();
+		glClear( GL_COLOR_BUFFER_BIT );
+
+		// Draw our first triangle
+		glUseProgram( shaderProgram );
+		glBindVertexArray( VAO );
+		glDrawArrays( GL_TRIANGLES, 0, 3 );
+		glBindVertexArray( 0 );
+
+		glfwSwapBuffers( window );
+	}
+
+	glDeleteVertexArrays( 1, &VAO );
+	glDeleteBuffers( 1, &VBO );
 
 	glfwDestroyWindow( window );
-    glfwTerminate();
-    return 0;
+	glfwTerminate();
+	return 0;
 }

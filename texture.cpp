d5e167aa1cd41d67583b83801f7446be805662b4
Richie Kirchofer <rgkirch@gmail.com>
2015-08-16 12:00:43 -0400

    flip on load!

--- texture.cpp
+++ texture.cpp
@@ -122,6 +122,7 @@ int main() {
 
 
 
+	stbi_set_flip_vertically_on_load(1);
 	// TEXTURE
     GLuint texture;
     glGenTextures(1, &texture);
@@ -139,7 +140,6 @@ int main() {
     glGenerateMipmap(GL_TEXTURE_2D);
     stbi_image_free(image);
     glBindTexture(GL_TEXTURE_2D, 0);
-
 
 
     GLuint texture1;

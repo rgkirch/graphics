8df222daa7b325f5de60e8a356933f87e68ef339
richie <rgkirch@gmail.com>
2017-07-01 11:30:19 -0400

    part1 still, circle of circles

--- cinder/src/cinder-test.cpp
+++ cinder/src/cinder-test.cpp
@@ -1,9 +1,12 @@
+#include <iostream>
 #include "cinder/app/App.h"
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
 
 using namespace ci;
 using namespace ci::app;
+using std::cout;
+using std::endl;
 
 auto red = Color(1,0,0);
 auto green = Color(0,1,0);
@@ -13,18 +16,23 @@ class BasicApp : public App {
 public:
     void draw() override {
         gl::clear();
-        auto center = getWindowCenter();
-        auto r = 70;
-        gl::setMatricesWindow( getWindowSize() );
-        gl::translate( getWindowCenter().x, 75 );
-        gl::color( red );
-        gl::drawSolidCircle( vec2{0}, r );
-        gl::translate( 0, 150 );
-        gl::color( green );
-        gl::drawSolidCircle( vec2{0}, r );
-        gl::translate( 0, 150 );
-        gl::color( blue );
-        gl::drawSolidCircle( vec2{0}, r );
+        gl::pushModelMatrix();
+        gl::translate( getWindowCenter() );
+        auto numCircles = 16;
+        auto radius = getWindowHeight() / 2.f - 30;
+        for(auto c = 0; c < numCircles; c++) {
+            auto rel = c / (float)numCircles;
+            auto angle = rel * M_PI * 2;
+            auto offset = vec2{ cos(angle), sin(angle) };
+            gl::pushModelMatrix();
+            gl::translate( offset * radius );
+            gl::color( Color{CM_HSV, rel, 1, 1} );
+            gl::drawStrokedCircle( vec2{}, 30 );
+            gl::popModelMatrix();
+        }
+        gl::color( Color{1,1,1} );
+        gl::drawSolidCircle( vec2{}, 15 );
+        gl::popModelMatrix();
     }
 };
 

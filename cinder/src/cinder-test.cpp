b75afa45df6c5fb41bb2888ec7da10630490dd7e
richie <rgkirch@gmail.com>
2017-07-01 17:21:49 -0400

    part 4 texture sphere

--- cinder/src/cinder-test.cpp
+++ cinder/src/cinder-test.cpp
@@ -9,25 +9,46 @@ using namespace ci::app;
 
 class BasicApp : public App {
 public:
-    void setup() override;
-    void draw() override;
+    void    setup() override;
+    void    draw() override;
 
-    gl::Texture2dRef    mTex;
+    CameraPersp         mCam;
+    gl::BatchRef        mSphere;
+    gl::TextureRef      mTexture;
+    gl::GlslProgRef     mGlsl;
 };
 
 void BasicApp::setup()
 {
-    auto img = loadImage( "/home/richie/Documents/rgkirch/glfw/cinder/lib/Cinder/docs/htmlsrc/guides/opengl/images/clouds.jpg" );
-    mTex = gl::Texture2d::create( img );
+    auto img = loadImage( "/home/richie/Documents/rgkirch/glfw/cinder/lib/Cinder/samples/_opengl/MipMap/resources/checkerboard.png" );
+    mTexture = gl::Texture::create( img );
+    mTexture->bind();
+
+    auto shader = gl::ShaderDef().texture().lambert();
+    mGlsl = gl::getStockShader( shader );
+    auto sphere = geom::Sphere().subdivisions( 50 );
+    mSphere = gl::Batch::create( sphere, mGlsl );
+
+    gl::enableDepthWrite();
+    gl::enableDepthRead();
 }
 
 void BasicApp::draw()
 {
-    gl::clear();
+    auto x = getElapsedFrames() / (4 * 60.f) * M_PI * 2;
+    mCam.lookAt( vec3( sin(x) * 4, 2, cos(x) * 4 ), vec3( 0 ) );
 
-    gl::draw( mTex );
+    gl::clear( Color( 0.2f, 0.2f, 0.2f ) );
+    gl::setMatrices( mCam );
+
+    mSphere->draw();
+
+    // draw the texture itself in the upper right corner
+    gl::setMatricesWindow( getWindowSize() );
+    Rectf drawRect( 0, 0, mTexture->getWidth() / 3,
+                    mTexture->getHeight() / 3 );
+    gl::draw( mTexture, drawRect );
 }
-
 
 CINDER_APP( BasicApp, RendererGl )
 

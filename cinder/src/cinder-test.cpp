4f4d366b7086ae1eb31ef86b3d47222eab881f8b
richie <rgkirch@gmail.com>
2017-07-01 17:09:56 -0400

    part 4 load image

--- cinder/src/cinder-test.cpp
+++ cinder/src/cinder-test.cpp
@@ -12,67 +12,22 @@ public:
     void setup() override;
     void draw() override;
 
-    static const int NUM_SLICES = 12;
-
-    CameraPersp     mCam;
-    gl::BatchRef    mSlices[NUM_SLICES];
+    gl::Texture2dRef    mTex;
 };
 
 void BasicApp::setup()
 {
-    auto lambert = gl::ShaderDef().lambert().color();
-    auto shader = gl::getStockShader( lambert );
-
-    for( int i = 0; i < NUM_SLICES; ++i ) {
-        float rel = i / (float)NUM_SLICES;
-        float sliceHeight = 1.0f / NUM_SLICES;
-        auto slice = geom::Cube().size( 1, sliceHeight, 1 );
-        auto trans = geom::Translate( 0, rel, 0 );
-        auto color = geom::Constant( geom::COLOR, Color( CM_HSV, rel, 1, 1 ) );
-        mSlices[i] = gl::Batch::create( slice >> trans >> color, shader );
-    }
-
-    mCam.lookAt( vec3( 2, 3, 2 ), vec3( 0, 0.5f, 0 ) );
+    auto img = loadImage( "/home/richie/Documents/rgkirch/glfw/cinder/lib/Cinder/docs/htmlsrc/guides/opengl/images/clouds.jpg" );
+    mTex = gl::Texture2d::create( img );
 }
 
 void BasicApp::draw()
 {
     gl::clear();
-    gl::enableDepthRead();
-    gl::enableDepthWrite();
 
-    gl::setMatrices( mCam );
-
-    const float delay = 0.25f;
-    // time in seconds for one slice to rotate
-    const float rotationTime = 1.5f;
-    // time in seconds to delay each slice's rotation
-    const float rotationOffset = 0.1f; // seconds
-    // total time for entire animation
-    const float totalTime = delay + rotationTime +
-                            NUM_SLICES * rotationOffset;
-
-    // loop every 'totalTime' seconds
-    float time = fmod( getElapsedFrames() / 30.0f, totalTime );
-
-    for( int i = 0; i < NUM_SLICES; ++i ) {
-        // animates from 0->1
-        float rotation = 0;
-        // when does the slice begin rotating
-        float startTime = i * rotationOffset;
-        // when does it complete
-        float endTime = startTime + rotationTime;
-        // are we in the middle of our time section?
-        if( time > startTime && time < endTime )
-            rotation = ( time - startTime ) / rotationTime;
-        // ease fn on rotation, then convert to radians
-        float angle = easeInOutQuint( rotation ) * M_PI / 2.0f;
-
-        gl::ScopedModelMatrix scpModelMtx;
-        gl::rotate( angleAxis( angle, vec3( 0, 1, 0 ) ) );
-        mSlices[i]->draw();
-    }
+    gl::draw( mTex );
 }
+
 
 CINDER_APP( BasicApp, RendererGl )
 

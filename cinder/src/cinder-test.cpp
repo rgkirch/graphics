d7670a5a43c34d42f30bc8b334b63cc862a359ef
richie <rgkirch@gmail.com>
2017-07-01 15:53:27 -0400

    part 3 nine shapes

--- cinder/src/cinder-test.cpp
+++ cinder/src/cinder-test.cpp
@@ -11,17 +11,35 @@ public:
     void setup() override;
     void draw() override;
 
-    CameraPersp     mCam;
-    gl::BatchRef    mBox;
+    CameraPersp		mCam;
+    gl::BatchRef	mShapes[3][3];
 };
 
 void BasicApp::setup()
 {
     auto lambert = gl::ShaderDef().lambert().color();
-    gl::GlslProgRef shader = gl::getStockShader( lambert );
-    mBox = gl::Batch::create( geom::Cube(), shader );
+    gl::GlslProgRef	shader = gl::getStockShader( lambert );
 
-    mCam.lookAt( vec3( 3, 4.5, 4.5 ), vec3( 0, 1, 0 ) );
+    auto capsule = geom::Capsule().subdivisionsAxis( 10 ).subdivisionsHeight( 10 );
+    mShapes[0][0] = gl::Batch::create( capsule, shader );
+    auto sphere = geom::Sphere().subdivisions( 30 );
+    mShapes[0][1] = gl::Batch::create( sphere, shader );
+    auto cylinder = geom::Cylinder().subdivisionsAxis( 40 ).subdivisionsHeight( 2 );
+    mShapes[0][2] = gl::Batch::create( cylinder, shader );
+    auto cube = geom::Cube();
+    mShapes[1][0] = gl::Batch::create( cube, shader );
+    auto cone = geom::Cone();
+    mShapes[1][1] = gl::Batch::create( cone, shader );
+    auto torus = geom::Torus();
+    mShapes[1][2] = gl::Batch::create( torus, shader );
+    auto helix = geom::Helix().subdivisionsAxis( 20 ).subdivisionsHeight( 10 );
+    mShapes[2][0] = gl::Batch::create( helix, shader );
+    auto icosahedron = geom::Icosahedron();
+    mShapes[2][1] = gl::Batch::create( icosahedron, shader );
+    auto teapot = geom::Teapot() >> geom::Scale( 1.5f );
+    mShapes[2][2] = gl::Batch::create( teapot, shader );
+
+    mCam.lookAt( vec3( 5, 11, 5 ), vec3( 0 ) );
 }
 
 void BasicApp::draw()
@@ -32,26 +50,18 @@ void BasicApp::draw()
 
     gl::setMatrices( mCam );
 
-    int numSpheres = 64;
-    float maxAngle = M_PI * 7;
-    float spiralRadius = 1;
-    float height = 2;
-    float boxSize = 0.05f;
-    float anim = getElapsedFrames() / 30.0f;
+    float gridSize = 5;
 
-    for( int s = 0; s < numSpheres; ++s ) {
-        float rel = s / (float)numSpheres;
-        float angle = rel * maxAngle;
-        float y = fabs( cos( rel * M_PI + anim ) ) * height;
-        float r = rel * spiralRadius;
-        vec3 offset( r * cos( angle ), y / 2, r * sin( angle ) );
+    for( int i = 0; i < 3; ++i ) {
+        for( int j = 0; j < 3; ++j ) {
+            float x = ( -0.5f + i / 2.0f ) * gridSize;
+            float z = ( -0.5f + j / 2.0f ) * gridSize;
 
-        gl::pushModelMatrix();
-        gl::translate( offset );
-        gl::scale( vec3( boxSize, y, boxSize ) );
-        gl::color( Color( CM_HSV, rel, 1, 1 ) );
-        mBox->draw();
-        gl::popModelMatrix();
+            gl::ScopedModelMatrix scpModelMatrix;
+            gl::translate( x, 1, z );
+            gl::color( i / 2.0f, 1 - i * j, j / 2.0f );
+            mShapes[i][j]->draw();
+        }
     }
 }
 

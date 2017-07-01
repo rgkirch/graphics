4da685a0d1597428214e0551bd91c1eba1f48db1
richie <rgkirch@gmail.com>
2017-07-01 15:28:29 -0400

    part 2 animation

--- cinder/src/cinder-test.cpp
+++ cinder/src/cinder-test.cpp
@@ -17,34 +17,33 @@ public:
         gl::enableDepthRead();
         gl::enableDepthWrite();
 
-        static long count = 0;
-        count++;
-        auto turn = count * M_PI * 2 / (60.f * 4);
+        auto turn = getElapsedFrames() * M_PI * 2 / (60.f * 4);
 
         CameraPersp cam;
-        cam.lookAt( vec3( sin(turn) * 6, 2.5f, cos(turn) * 6 ), vec3( 0, 1.5f, 0 ) );
+        cam.lookAt( vec3( sin(turn) * 4.5, 4.5f, cos(turn) * 4.5f ), vec3( 0, 1, 0 ) );
         gl::setMatrices( cam );
 
         auto lambert = gl::ShaderDef().lambert().color();
         auto shader = gl::getStockShader( lambert );
         shader->bind();
 
-        auto numSpheres = 64;
-        auto maxAngle = M_PI * 7.f;
-        auto spiralRadius = 1.f;
-        auto height = 3.f;
+        int numSpheres = 64;
+        float maxAngle = M_PI * 7;
+        float spiralRadius = 1;
+        float height = 2;
+        float anim = getElapsedFrames() / 30.0f;
 
-        for( auto s = 0; s < numSpheres; ++s ) {
-            auto rel = s / (float)numSpheres;
-            auto angle = rel * maxAngle;
-            auto y = rel * height;
-            auto r = rel * spiralRadius * spiralRadius;
-            vec3 offset( r * cos( angle ), y, r * sin( angle ) );
-
+        for( int s = 0; s < numSpheres; ++s ) {
+            float rel = s / (float)numSpheres;
+            float angle = rel * maxAngle;
+            float y = fabs( cos( rel * M_PI + anim ) ) * height;
+            float r = rel * spiralRadius;
+            vec3 offset( r * cos( angle ), y / 2, r * sin( angle ) );
             gl::pushModelMatrix();
             gl::translate( offset );
+            gl::scale( vec3( 0.05f, y, 0.05f ) );
             gl::color( Color( CM_HSV, rel, 1, 1 ) );
-            gl::drawCube( vec3{}, vec3{0.1f});
+            gl::drawCube( vec3(), vec3( 1 ) );
             gl::popModelMatrix();
         }
     }

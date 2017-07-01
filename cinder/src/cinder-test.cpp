3e3bf54b9a0aacb9ce1bae646af9bf28e7b95a20
richie <rgkirch@gmail.com>
2017-07-01 13:40:14 -0400

    part 2 spiral of multicolored dots

--- cinder/src/cinder-test.cpp
+++ cinder/src/cinder-test.cpp
@@ -13,25 +13,40 @@ auto blue = Color(0,0,1);
 class BasicApp : public App {
 public:
     void draw() override {
-        static long count = 0;
-//        auto now = std::chrono::system_clock::now();
-//        auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count();
-//        auto turn = ms / 10000.f;
-        count++;
-        auto turn = count * M_PI * 2 / (60.f * 4);
         gl::clear();
         gl::enableDepthRead();
         gl::enableDepthWrite();
 
+        static long count = 0;
+        count++;
+        auto turn = count * M_PI * 2 / (60.f * 4);
+
         CameraPersp cam;
-        cam.lookAt( vec3( sin(turn) * 5, 2, cos(turn) * 5 ), vec3( 0 ) );
+        cam.lookAt( vec3( sin(turn) * 6, 2.5f, cos(turn) * 6 ), vec3( 0, 1.5f, 0 ) );
         gl::setMatrices( cam );
 
-        auto lambert = gl::ShaderDef().lambert();
+        auto lambert = gl::ShaderDef().lambert().color();
         auto shader = gl::getStockShader( lambert );
         shader->bind();
-        gl::drawSphere( vec3(), 1.0f );
-        console() << "frame" << std::endl;
+
+        auto numSpheres = 64;
+        auto maxAngle = M_PI * 7.f;
+        auto spiralRadius = 1.f;
+        auto height = 3.f;
+
+        for( auto s = 0; s < numSpheres; ++s ) {
+            auto rel = s / (float)numSpheres;
+            auto angle = rel * maxAngle;
+            auto y = rel * height;
+            auto r = rel * spiralRadius * spiralRadius;
+            vec3 offset( r * cos( angle ), y, r * sin( angle ) );
+
+            gl::pushModelMatrix();
+            gl::translate( offset );
+            gl::color( Color( CM_HSV, rel, 1, 1 ) );
+            gl::drawSphere( vec3(), 0.1f, 30 );
+            gl::popModelMatrix();
+        }
     }
 };
 

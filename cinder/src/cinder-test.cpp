1727ee7694dec049e37ec7b0097ee2b2a4daffe6
richie <rgkirch@gmail.com>
2017-07-01 18:11:29 -0400

    part 5, bunch of planes

--- cinder/src/cinder-test.cpp
+++ cinder/src/cinder-test.cpp
@@ -12,41 +12,34 @@ public:
     void	setup() override;
     void	draw() override;
 
-    CameraPersp         mCam;
-    gl::BatchRef        mRect;
+    CameraPersp			mCam;
+    gl::BatchRef		mRect;
     gl::GlslProgRef		mGlsl;
 };
 
 void BasicApp::setup()
 {
-    mCam.lookAt( vec3( 2, 1, 3 ), vec3( 0 ) );
+    mCam.lookAt( vec3( 3, 2, 3 ), vec3( 0 ) );
 
     mGlsl = gl::GlslProg::create( gl::GlslProg::Format()
                                           .vertex(	CI_GLSL( 150,
                                                                uniform mat4	ciModelViewProjection;
                                                                        in vec4			ciPosition;
-                                                                       in vec4			ciColor;
-                                                                       out vec4		Color;
 
                                                                        void main( void ) {
                                                                            gl_Position	= ciModelViewProjection * ciPosition;
-                                                                           Color = ciColor;
                                                                        }
                                                       ) )
                                           .fragment(	CI_GLSL( 150,
-                                                                 in vec4		Color;
-                                                                         out vec4	oColor;
+                                                                 uniform vec4	uColor;
+                                                                         out vec4		oColor;
 
                                                                          void main( void ) {
-                                                                             oColor = Color;
+                                                                             oColor = uColor;
                                                                          }
                                                         ) ) );
 
-    auto rect = geom::Rect().colors( Color( 1, 0, 0 ),
-                                     Color( 0, 0, 1 ),
-                                     Color( 0, 0, 1 ),
-                                     Color( 1, 0, 0 ) );
-    mRect = gl::Batch::create( rect, mGlsl );
+    mRect = gl::Batch::create( geom::Plane(), mGlsl );
 
     gl::enableDepthWrite();
     gl::enableDepthRead();
@@ -56,7 +49,18 @@ void BasicApp::draw()
 {
     gl::clear( Color( 0.2f, 0.2f, 0.2f ) );
     gl::setMatrices( mCam );
-    mRect->draw();
+
+    const int NUM_PLANES = 30;
+    for( int p = 0; p < NUM_PLANES; ++p ) {
+        float hue = p / (float)NUM_PLANES;
+        ColorAf color( CM_HSV, hue, 1, 1, 1 );
+        mGlsl->uniform( "uColor", color );
+
+        gl::ScopedModelMatrix scpMtx;
+        float angle = M_PI * p / (float)NUM_PLANES;
+        gl::rotate( angleAxis( angle, vec3( 1, 0, 0 ) ) );
+        mRect->draw();
+    }
 }
 
 

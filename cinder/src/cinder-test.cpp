f0bae5eef3dec370f8000f6490cba77d50133e05
richie <rgkirch@gmail.com>
2017-07-01 13:10:05 -0400

    part 2, lambert example with camera movement

--- cinder/src/cinder-test.cpp
+++ cinder/src/cinder-test.cpp
@@ -1,12 +1,10 @@
-#include <iostream>
+#include <chrono>
 #include "cinder/app/App.h"
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
 
 using namespace ci;
 using namespace ci::app;
-using std::cout;
-using std::endl;
 
 auto red = Color(1,0,0);
 auto green = Color(0,1,0);
@@ -15,11 +13,21 @@ auto blue = Color(0,0,1);
 class BasicApp : public App {
 public:
     void draw() override {
+        static long count = 0;
+//        auto now = std::chrono::system_clock::now();
+//        auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count();
+//        auto turn = ms / 10000.f;
+        count++;
+        auto turn = count * M_PI * 2 / 60.f;
         gl::clear();
         CameraPersp cam;
-        cam.lookAt( vec3( 3, 3, 3 ), vec3( 0 ) );
+        cam.lookAt( vec3( sin(turn), cos(turn), 3 ), vec3( 0 ) );
         gl::setMatrices( cam );
-        gl::drawCube( vec3(), vec3( 2 ) );
+        auto lambert = gl::ShaderDef().lambert();
+        auto shader = gl::getStockShader( lambert );
+        shader->bind();
+        gl::drawSphere( vec3(), 1.0f );
+        console() << "frame" << std::endl;
     }
 };
 

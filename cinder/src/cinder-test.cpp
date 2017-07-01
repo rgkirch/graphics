9839bcc7f0b78c9fba081fcf843361da6b702aca
richie <rgkirch@gmail.com>
2017-07-01 10:17:29 -0400

    three triangles

--- cinder/src/cinder-test.cpp
+++ cinder/src/cinder-test.cpp
@@ -5,11 +5,22 @@
 using namespace ci;
 using namespace ci::app;
 
+auto red = Color(1,0,0);
+auto green = Color(0,1,0);
+auto blue = Color(0,0,1);
+
 class BasicApp : public App {
 public:
     void draw() override {
         gl::clear();
-        gl::drawSolidCircle( getWindowCenter(), 200 );
+        auto center = getWindowCenter();
+        float r = 100;
+        gl::color(red);
+        gl::drawSolidCircle( center + vec2{-r, r}, r );
+        gl::color(green);
+        gl::drawSolidCircle( center + vec2{r, r}, r );
+        gl::color(blue);
+        gl::drawSolidCircle( center + vec2{0, -.73 * r}, r );
     }
 };
 

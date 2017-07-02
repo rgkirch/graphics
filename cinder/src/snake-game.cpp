26c863ece98cf27414fbc2162f49fb5b1eebd5c0
richie <rgkirch@gmail.com>
2017-07-01 20:23:03 -0400

    gonna make a snake game

---
+++ cinder/src/snake-game.cpp
@@ -0,0 +1,41 @@
+#include "cinder/app/App.h"
+#include "cinder/app/RendererGl.h"
+#include "cinder/gl/gl.h"
+
+using namespace ci;
+using namespace ci::app;
+using std::cout;
+using std::endl;
+
+auto red = Color(1,0,0);
+auto green = Color(0,1,0);
+auto blue = Color(0,0,1);
+
+class BasicApp : public App {
+public:
+    void draw() override;
+};
+
+void BasicApp::draw() {
+    gl::clear();
+    gl::pushModelMatrix();
+    gl::translate( getWindowCenter() );
+    auto numCircles = 16;
+    auto radius = getWindowHeight() / 2.f - 30;
+    for(auto c = 0; c < numCircles; c++) {
+        auto rel = c / (float)numCircles;
+        auto angle = rel * M_PI * 2;
+        auto offset = vec2{ cos(angle), sin(angle) };
+        gl::pushModelMatrix();
+        gl::translate( offset * radius );
+        gl::rotate( angle );
+        gl::scale( 8, .25f );
+        gl::color( Color{CM_HSV, rel, 1, 1} );
+        gl::drawSolidCircle( vec2{}, 30 );
+        gl::popModelMatrix();
+    }
+    gl::popModelMatrix();
+}
+
+CINDER_APP( BasicApp, RendererGl )
+

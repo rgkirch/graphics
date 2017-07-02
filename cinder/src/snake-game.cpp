fdf6fc82a0070e2c98465ec9c9b8b24c957eb67c
richie <rgkirch@gmail.com>
2017-07-02 14:21:59 -0400

    ok, the squares fill up the space

--- cinder/src/snake-game.cpp
+++ cinder/src/snake-game.cpp
@@ -19,6 +19,7 @@ public:
     CameraPersp     mCam;
     gl::GlslProgRef	mShader;
     std::vector<gl::BatchRef> mTile;
+    float tileScale;
     int tilesWide;
     int tilesHigh;
 };
@@ -40,18 +41,18 @@ void BasicApp::setup()
     XmlTree doc(loadFile( "/home/richie/Documents/rgkirch/glfw/cinder/assets/values.xml" ));
     tilesWide = atoi(doc.getChild("snakeGame/tilesWide").getValue().c_str());
     tilesHigh = atoi(doc.getChild("snakeGame/tilesHigh").getValue().c_str());
+    tileScale = atof(doc.getChild("snakeGame/tilesScale").getValue().c_str());
     mShader = gl::getStockShader( gl::ShaderDef().lambert().color() );
     mCam.lookAt( vec3( 0, 0, 3 ), vec3( 0, 0, 0 ) );
     mTile.clear();
-    assert(mTile.size() == 0);
     mTile.reserve(tilesWide * tilesHigh);
     int i = 0;
     for(auto x = 1.f / (tilesWide + 1); x < 1; x += 1.f / (tilesWide + 1)) {
         for(auto y = 1.f / (tilesHigh + 1); y < 1; y += 1.f / (tilesHigh + 1)) {
             auto c = Color(CM_HSV, i / (float)(tilesWide * tilesHigh), 1, 1 );
             auto color = geom::Constant(geom::COLOR, c);
-            auto trans = geom::Translate( x, y, 0 );
-            auto scale = geom::Scale(.1f);
+            auto trans = geom::Translate( x * 2 - 1, y * 2 - 1, 0 );
+            auto scale = geom::Scale(tileScale);
             mTile.push_back(gl::Batch::create( geom::Cube() >> scale >> trans >> color, mShader));
             i++;
         }

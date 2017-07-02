d8c5a9cc1cbd4a45bdd204144307f30267edf368
richie <rgkirch@gmail.com>
2017-07-01 20:07:42 -0400

    part 5 fragment makes plane a checker pattern while height is sin x cosy

--- cinder/src/cinder-test.cpp
+++ cinder/src/cinder-test.cpp
@@ -6,53 +6,64 @@
 using namespace ci;
 using namespace ci::app;
 
-
 class BasicApp : public App {
 public:
     void	setup() override;
     void	draw() override;
 
-    CameraPersp			mCam;
-    gl::BatchRef		mRect;
-    gl::GlslProgRef		mGlsl;
+    CameraPersp             mCam;
+    gl::BatchRef            mRect;
+    gl::GlslProgRef         mGlsl;
     gl::Texture2dRef        mTex;
 };
-
-
 
 void BasicApp::setup()
 {
     mCam.lookAt( vec3( 3, 2, 3 ), vec3( 0 ) );
 
-    auto img = loadImage( "/home/richie/Documents/rgkirch/glfw/cinder/lib/Cinder/samples/_opengl/Cube/assets/texture.jpg" );
-    mTex = gl::Texture2d::create( img );
-    mTex->bind( 0 );
-
     mGlsl = gl::GlslProg::create( gl::GlslProg::Format()
-                                          .vertex(	CI_GLSL( 150,
-                                                               uniform mat4	ciModelViewProjection;
-                                                                       in vec4			ciPosition;
-                                                                       in vec2			ciTexCoord0;
-                                                                       out vec2		TexCoord0;
+          .vertex(	CI_GLSL( 150,
+               uniform mat4	ciModelViewProjection;
+                   in vec4			ciPosition;
+                   in vec2			ciTexCoord0;
+                   out vec2		TexCoord0;
 
-                                                                       void main( void ) {
-                                                                           gl_Position	= ciModelViewProjection * ciPosition;
-                                                                           TexCoord0 = ciTexCoord0;
-                                                                       }
-                                                      ) )
-                                          .fragment(	CI_GLSL( 150,
-                                                                 uniform vec4		uColor;
-                                                                         uniform sampler2D	uTex0;
+                   float offset( vec2 uv )
+                   {
+                   return ( sin( uv.x * 15.0 ) +
+                        cos( uv.y * 7.0f + uv.x * 13.0f ) ) * 0.1f;
+                   }
 
-                                                                         in vec2				TexCoord0;
-                                                                         out vec4			oColor;
+                   void main( void ) {
+                   vec4 pos = ciPosition;
+                   pos.y = offset( ciTexCoord0 );
+                   gl_Position	= ciModelViewProjection * pos;
+                   TexCoord0 = ciTexCoord0;
+                   }
+          ) )
+          .fragment(	CI_GLSL( 150,
+            uniform float		uCheckSize;
 
-                                                                         void main( void ) {
-                                                                             oColor = texture( uTex0, TexCoord0 ) * uColor;
-                                                                         }
-                                                        ) ) );
+                in vec2				TexCoord0;
+                out vec4			oColor;
 
-    mRect = gl::Batch::create( geom::Plane(), mGlsl );
+                vec4 checker( vec2 uv )
+                {
+                    float v = floor( uCheckSize * uv.x ) +
+                          floor( uCheckSize * uv.y );
+                    if( mod( v, 2.0 ) < 1.0 )
+                    return vec4( 1, 1, 1, 1 );
+                    else
+                    return vec4( 0, 0, 0, 1 );
+                }
+
+                  void main( void ) {
+                      oColor = checker( TexCoord0 );
+                  }
+              ) ) );
+
+    auto plane = geom::Plane().subdivisions( ivec2( 30 ) );
+    mRect = gl::Batch::create( plane, mGlsl );
 
     gl::enableDepthWrite();
     gl::enableDepthRead();
@@ -60,25 +71,11 @@ void BasicApp::setup()
 
 void BasicApp::draw()
 {
-    gl::clear( Color( 0.2f, 0.2f, 0.2f ) );
+    gl::clear( Color( 0.2f, 0.2f, 0.3f ) );
     gl::setMatrices( mCam );
 
-    const int NUM_PLANES = 7;
-    for( int p = 0; p < NUM_PLANES; ++p ) {
-        float hue = p / (float)NUM_PLANES;
-        ColorAf color( CM_HSV, hue, 1, 1, 1 );
-        mGlsl->uniform( "uTex0", 0 );
-        mGlsl->uniform( "uColor", color );
-
-        gl::ScopedModelMatrix scpMtx;
-        float angle = M_PI * p / (float)NUM_PLANES;
-        gl::rotate( angleAxis( angle, vec3( 1, 0, 0 ) ) );
-        mRect->draw();
-    }
-
-    gl::setMatricesWindow( getWindowSize() );
-    gl::draw( mTex, Rectf( 0, getWindowHeight() - 100,
-                           150, getWindowHeight() ) );
+    mGlsl->uniform( "uCheckSize", 30.0f );
+    mRect->draw();
 }
 
 

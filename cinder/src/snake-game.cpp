fd4a2b146e74d5b505a249d52d9e9aada7d5b93b
richie <rgkirch@gmail.com>
2017-07-02 12:59:13 -0400

    i'm missing one box and there's no shading

--- cinder/src/snake-game.cpp
+++ cinder/src/snake-game.cpp
@@ -24,18 +24,15 @@ public:
 
 void BasicApp::setup()
 {
-    gl::enableDepthRead();
-    gl::enableDepthWrite();
-
     mShader = gl::getStockShader( gl::ShaderDef().lambert().color() );
-    mCam.lookAt( vec3( 5, 5, 5 ), vec3( 0, 0, 0 ) );
+    mCam.lookAt( vec3( 3, 3, 3 ), vec3( 0, 0, 0 ) );
     int i = 0;
     for(auto x = 1.f / (tilesWide + 1); x < 1; x += 1.f / (tilesWide + 1)) {
         for(auto y = 1.f / (tilesHigh + 1); y < 1; y += 1.f / (tilesHigh + 1)) {
-//            auto color = geom::Constant( geom::COLOR, Color( CM_HSV, i / (float)mTile.size(), 1, 1 ) );
-//            auto trans = geom::Translate( x, y, 0 );
-//            auto scale = geom::Scale(.2f);
-            mTile.push_back(gl::Batch::create( geom::Cube() /*>> trans >> scale >> color*/, mShader));
+            auto color = geom::Constant( geom::COLOR, Color( CM_HSV, i / (float)mTile.size(), 1, 1 ) );
+            auto trans = geom::Translate( x*2, y*2, 0 );
+            auto scale = geom::Scale(.1f);
+            mTile.push_back(gl::Batch::create( geom::Cube() >> scale >> trans >> color, mShader));
             i++;
         }
     }
@@ -43,10 +40,13 @@ void BasicApp::setup()
 
 void BasicApp::draw() {
     gl::clear();
+    gl::enableDepthRead();
+    gl::enableDepthWrite();
+
     gl::setMatrices(mCam);
 
     gl::ScopedModelMatrix scpModelMtx;
-    gl::color(.3f,.3f,1);
+//    gl::color(.3f,.3f,1);
     for (int i = 0; i < mTile.size(); i++) {
         mTile[i]->draw();
     }

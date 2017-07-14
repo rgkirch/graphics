4a4c7e73bc2aeb77f4d59d0906657a03f565421c
Richie Kirchofer <rgkirch@gmail.com>
2017-07-14 07:35:47 -0400

    auto format

--- cinder/src/bitcoin-price-history/graph.cpp
+++ cinder/src/bitcoin-price-history/graph.cpp
@@ -1,4 +1,3 @@
-#include <algorithm>
 #include "graph.hpp"
 #include "bitcoin.hpp"
 #include "json.hpp"
@@ -6,50 +5,50 @@
 using json = nlohmann::json;
 
 void BitcoinPriceHistory::setup() {
-    mShader = gl::getStockShader( gl::ShaderDef().lambert().color() );
-    mCam.lookAt( vec3( 0, 0, 3 ), vec3( 0, 0, 0 ) );
-
+    mShader = gl::getStockShader(gl::ShaderDef().lambert().color());
+    mCam.lookAt(vec3(0, 0, 3), vec3(0, 0, 0));
 
     HTTPDownloader downloader;
-    auto content = downloader.download("https://poloniex.com/public?command=returnChartData&currencyPair=USDT_BTC&start=1496970103&end=9999999999&period=86400");
+    auto content =
+            downloader.download("https://poloniex.com/"
+                                        "public?command=returnChartData&currencyPair=USDT_"
+                                        "BTC&start=1496970103&end=9999999999&period=86400");
     auto j = json::parse(content);
-    for(auto r : j) {
-        for(auto k : dataKeys) {
+    for (auto r : j) {
+        for (auto k : dataKeys) {
             data[k].push_back(r[k]);
         }
     }
-    double max = *std::max_element(std::begin(data["close"]), std::end(data["close"]));
+    double max =
+            *std::max_element(std::begin(data["close"]), std::end(data["close"]));
     std::vector<float> pixelHeights;
-    std::transform(
-            std::begin(data["close"]),
-            std::end(data["close"]),
-            std::back_inserter(pixelHeights),
-            std::bind(
-                    [](double d, double max, int windowHeight)->double {
-                        return (1 - (d / max)) * (windowHeight - 1);
-                    }
-                    ,std::placeholders::_1
-                    ,max
-                    ,getWindowHeight()
-            )
-    );
-    path.moveTo( vec2{0, pixelHeights.front()} );
+    std::transform(std::begin(data["close"]), std::end(data["close"]),
+                   std::back_inserter(pixelHeights),
+                   std::bind(
+                           [](double d, double max, int windowHeight) -> double {
+                               return (1 - (d / max)) * (windowHeight - 1);
+                           },
+                           std::placeholders::_1, max, getWindowHeight()));
+    path.moveTo(vec2{0, pixelHeights.front()});
     for (int i = 1; i < pixelHeights.size(); i++) {
-        auto v =  vec2{(float)i / pixelHeights.size() * getWindowWidth(), pixelHeights[i]};
+        auto v = vec2{(float) i / pixelHeights.size() * getWindowWidth(),
+                      pixelHeights[i]};
         console() << v << std::endl;
-        path.lineTo( v );
+        path.lineTo(v);
     }
 }
+
 void BitcoinPriceHistory::mouseDown(MouseEvent event) {
     console() << vec2{event.getX(), event.getY()} << std::endl;
 }
 
 void BitcoinPriceHistory::draw() {
     gl::clear();
-    gl::setMatricesWindow( getWindowSize() );
-    gl::color( Color::white() );
-    gl::draw( path );
+    gl::setMatricesWindow(getWindowSize());
+    gl::color(Color::white());
+    gl::draw(path);
 }
+
 void BitcoinPriceHistory::resize() {
-    mCam.setAspectRatio( getWindowAspectRatio() );
+    mCam.setAspectRatio(getWindowAspectRatio());
 }
